3345c493fe08fc6703cb67c1d1f9e4284523afb2
shaina <iamshainasabarwal@gmail.com>
2021-12-02 11:04:06 +0530

    developing work design

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,7 +3,6 @@ import '../styles/styles.scss';
 import '../styles/work.scss';
 import Header from '../components/header';
 import Footer from '../components/footer';
-import WorkFrame from '../components/workFrame';
 
 function Work() {
   return (
@@ -11,11 +10,16 @@ function Work() {
       <div className="container" >
         <Header />
         <div class="work-container">
-        <div class="work-tagline ff-brandon-light">We design, build and ship digital products on time with
+        <div class="work-tagline ff-brandon">We design, build and ship digital products on time with
           written guarantees on quality & performance</div>
-            <div class="work-blocks">
-              <WorkFrame />
-              <WorkFrame />
+          <div class="work-blocks">
+            <div class="work-blk ff-brandon">
+              <div class="work-num ff-brandon-bold">01</div>
+              <div class="work-title">Forby</div>             
+              <div class="work-line ff-brandon-light">Designing a minimal and user friendly for a community
+                where every opinion matters
+              </div>
+            </div>
             </div>
         </div>
         <Footer />
