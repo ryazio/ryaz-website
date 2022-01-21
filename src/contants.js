e2647440af2f79caaf96124dfdb5a32dde83947b
Rajvir Singh <rsaggu99@gmail.com>
2022-01-21 16:57:53 +0530

    Added Projects and its images

--- src/contants.js
+++ src/contants.js
@@ -82,6 +82,23 @@ import PUERTO_RICO_SLIDER_10 from './images/projects/puerto-rico/slider_10.png';
 import PUERTO_RICO_SLIDER_11 from './images/projects/puerto-rico/slider_11.png';
 import PUERTO_RICO_SLIDER_12 from './images/projects/puerto-rico/slider_12.png';
 
+import PUERTO_RICO_GUIDE_TITLE from './images/projects/puerto-rico-guide/title.png';
+import PUERTO_RICO_GUIDE_STACK_1 from './images/projects/puerto-rico-guide/stack_1.png';
+import PUERTO_RICO_GUIDE_STACK_2 from './images/projects/puerto-rico-guide/stack_2.png';
+import PUERTO_RICO_GUIDE_STACK_3 from './images/projects/puerto-rico-guide/stack_3.png';
+import PUERTO_RICO_GUIDE_STACK_4 from './images/projects/puerto-rico-guide/stack_4.png';
+import PUERTO_RICO_GUIDE_STACK_5 from './images/projects/puerto-rico-guide/stack_5.png';
+import PUERTO_RICO_GUIDE_STACK_6 from './images/projects/puerto-rico-guide/stack_6.png';
+import PUERTO_RICO_GUIDE_STACK_7 from './images/projects/puerto-rico-guide/stack_7.png';
+import PUERTO_RICO_GUIDE_STACK_8 from './images/projects/puerto-rico-guide/stack_8.png';
+import PUERTO_RICO_GUIDE_SLIDER_1 from './images/projects/puerto-rico-guide/slider_1.png';
+import PUERTO_RICO_GUIDE_SLIDER_2 from './images/projects/puerto-rico-guide/slider_2.png';
+import PUERTO_RICO_GUIDE_SLIDER_3 from './images/projects/puerto-rico-guide/slider_3.png';
+import PUERTO_RICO_GUIDE_SLIDER_4 from './images/projects/puerto-rico-guide/slider_4.png';
+import PUERTO_RICO_GUIDE_SLIDER_5 from './images/projects/puerto-rico-guide/slider_5.png';
+import PUERTO_RICO_GUIDE_SLIDER_6 from './images/projects/puerto-rico-guide/slider_6.png';
+import PUERTO_RICO_GUIDE_SLIDER_7 from './images/projects/puerto-rico-guide/slider_7.png';
+
 import WE_PATH_TITLE from './images/projects/we-path/title.png';
 import WE_PATH_STACK_1 from './images/projects/we-path/stack_1.png';
 import WE_PATH_STACK_2 from './images/projects/we-path/stack_2.png';
@@ -89,6 +106,92 @@ import WE_PATH_STACK_3 from './images/projects/we-path/stack_3.png';
 import WE_PATH_STACK_4 from './images/projects/we-path/stack_4.png';
 import WE_PATH_STACK_5 from './images/projects/we-path/stack_5.png';
 import WE_PATH_STACK_6 from './images/projects/we-path/stack_6.png';
+
+import AC_NAUTICA_TITLE from './images/projects/ac-nautica/title.png';
+import AC_NAUTICA_STACK_1 from './images/projects/ac-nautica/stack_1.png';
+import AC_NAUTICA_STACK_2 from './images/projects/ac-nautica/stack_2.png';
+import AC_NAUTICA_STACK_3 from './images/projects/ac-nautica/stack_3.png';
+import AC_NAUTICA_STACK_4 from './images/projects/ac-nautica/stack_4.png';
+import AC_NAUTICA_STACK_5 from './images/projects/ac-nautica/stack_5.png';
+import AC_NAUTICA_SLIDER_1 from './images/projects/ac-nautica/slider_1.png';
+import AC_NAUTICA_SLIDER_2 from './images/projects/ac-nautica/slider_2.png';
+import AC_NAUTICA_SLIDER_3 from './images/projects/ac-nautica/slider_3.png';
+import AC_NAUTICA_SLIDER_4 from './images/projects/ac-nautica/slider_4.png';
+import AC_NAUTICA_SLIDER_5 from './images/projects/ac-nautica/slider_5.png';
+import AC_NAUTICA_SLIDER_6 from './images/projects/ac-nautica/slider_6.png';
+
+import ALIBALANCE_TITLE from './images/projects/alibalance/alibalance_title.png';
+import ALIBALANCE_STACK_1 from './images/projects/alibalance/alibalance_stack_1.png';
+import ALIBALANCE_STACK_2 from './images/projects/alibalance/alibalance_stack_2.png';
+import ALIBALANCE_STACK_3 from './images/projects/alibalance/alibalance_stack_3.png';
+import ALIBALANCE_STACK_4 from './images/projects/alibalance/alibalance_stack_4.png';
+import ALIBALANCE_STACK_5 from './images/projects/alibalance/alibalance_stack_5.png';
+
+import GREEN_CLINIC_TITLE from './images/projects/green-clinic/title.png';
+import GREEN_CLINIC_STACK_1 from './images/projects/green-clinic/stack_1.png';
+import GREEN_CLINIC_STACK_2 from './images/projects/green-clinic/stack_2.png';
+import GREEN_CLINIC_STACK_3 from './images/projects/green-clinic/stack_3.png';
+import GREEN_CLINIC_STACK_4 from './images/projects/green-clinic/stack_4.png';
+import GREEN_CLINIC_SLIDER_1 from './images/projects/green-clinic/slider_1.png';
+import GREEN_CLINIC_SLIDER_2 from './images/projects/green-clinic/slider_2.png';
+import GREEN_CLINIC_SLIDER_3 from './images/projects/green-clinic/slider_3.png';
+import GREEN_CLINIC_SLIDER_4 from './images/projects/green-clinic/slider_4.png';
+import GREEN_CLINIC_SLIDER_5 from './images/projects/green-clinic/slider_5.png';
+import GREEN_CLINIC_SLIDER_6 from './images/projects/green-clinic/slider_6.png';
+import GREEN_CLINIC_SLIDER_7 from './images/projects/green-clinic/slider_7.png';
+import GREEN_CLINIC_SLIDER_8 from './images/projects/green-clinic/slider_8.png';
+import GREEN_CLINIC_SLIDER_9 from './images/projects/green-clinic/slider_9.png';
+
+import ITBI_TITLE from './images/projects/itbi/title.png';
+import ITBI_STACK_1 from './images/projects/itbi/stack_1.png';
+import ITBI_STACK_2 from './images/projects/itbi/stack_2.png';
+import ITBI_STACK_3 from './images/projects/itbi/stack_3.png';
+import ITBI_STACK_4 from './images/projects/itbi/stack_4.png';
+import ITBI_STACK_5 from './images/projects/itbi/stack_5.png';
+import ITBI_SLIDER_1 from './images/projects/itbi/slider_1.png';
+import ITBI_SLIDER_2 from './images/projects/itbi/slider_2.png';
+import ITBI_SLIDER_3 from './images/projects/itbi/slider_3.png';
+import ITBI_SLIDER_4 from './images/projects/itbi/slider_4.png';
+import ITBI_SLIDER_5 from './images/projects/itbi/slider_5.png';
+
+import SKILLSHAPE_TITLE from './images/projects/skillshape/title.png';
+import SKILLSHAPE_STACK_1 from './images/projects/skillshape/stack_1.png';
+import SKILLSHAPE_STACK_2 from './images/projects/skillshape/stack_2.png';
+import SKILLSHAPE_STACK_3 from './images/projects/skillshape/stack_3.png';
+import SKILLSHAPE_STACK_4 from './images/projects/skillshape/stack_4.png';
+import SKILLSHAPE_STACK_5 from './images/projects/skillshape/stack_5.png';
+import SKILLSHAPE_STACK_6 from './images/projects/skillshape/stack_6.png';
+import SKILLSHAPE_STACK_7 from './images/projects/skillshape/stack_7.png';
+import SKILLSHAPE_SLIDER_1 from './images/projects/skillshape/slider_1.png';
+import SKILLSHAPE_SLIDER_2 from './images/projects/skillshape/slider_2.png';
+import SKILLSHAPE_SLIDER_3 from './images/projects/skillshape/slider_3.png';
+import SKILLSHAPE_SLIDER_4 from './images/projects/skillshape/slider_4.png';
+import SKILLSHAPE_SLIDER_5 from './images/projects/skillshape/slider_5.png';
+
+import STTM_DESKTOP_TITLE from './images/projects/sttm-desktop/title.png';
+import STTM_DESKTOP_STACK_1 from './images/projects/sttm-desktop/stack_1.png';
+import STTM_DESKTOP_STACK_2 from './images/projects/sttm-desktop/stack_2.png';
+import STTM_DESKTOP_STACK_3 from './images/projects/sttm-desktop/stack_3.png';
+import STTM_DESKTOP_STACK_4 from './images/projects/sttm-desktop/stack_4.png';
+import STTM_DESKTOP_STACK_5 from './images/projects/sttm-desktop/stack_5.png';
+
+import DEMOBUTLER_TITLE from './images/projects/demobutler/title.png';
+import DEMOBUTLER_STACK_1 from './images/projects/demobutler/stack_1.png';
+import DEMOBUTLER_STACK_2 from './images/projects/demobutler/stack_2.png';
+import DEMOBUTLER_STACK_3 from './images/projects/demobutler/stack_3.png';
+import DEMOBUTLER_STACK_4 from './images/projects/demobutler/stack_4.png';
+import DEMOBUTLER_STACK_5 from './images/projects/demobutler/stack_5.png';
+import DEMOBUTLER_STACK_6 from './images/projects/demobutler/stack_6.png';
+import DEMOBUTLER_STACK_7 from './images/projects/demobutler/stack_7.png';
+import DEMOBUTLER_STACK_8 from './images/projects/demobutler/stack_8.png';
+import DEMOBUTLER_STACK_9 from './images/projects/demobutler/stack_9.png';
+import DEMOBUTLER_STACK_10 from './images/projects/demobutler/stack_10.png';
+import DEMOBUTLER_STACK_11 from './images/projects/demobutler/stack_11.png';
+import DEMOBUTLER_STACK_12 from './images/projects/demobutler/stack_12.png';
+import DEMOBUTLER_STACK_13 from './images/projects/demobutler/stack_13.png';
+import DEMOBUTLER_STACK_14 from './images/projects/demobutler/stack_14.png';
+import DEMOBUTLER_STACK_15 from './images/projects/demobutler/stack_15.png';
+import DEMOBUTLER_STACK_16 from './images/projects/demobutler/stack_16.jpg';
 
 export const employeeImageMapper = (employeeId) => {
   switch (employeeId) {
@@ -269,6 +372,39 @@ export const projectImageMapper = (imageName) => {
     case 'PUERTO_RICO_SLIDER_12':
       return PUERTO_RICO_SLIDER_12;
 
+    case 'PUERTO_RICO_GUIDE_TITLE':
+      return PUERTO_RICO_GUIDE_TITLE;
+    case 'PUERTO_RICO_GUIDE_STACK_1':
+      return PUERTO_RICO_GUIDE_STACK_1;
+    case 'PUERTO_RICO_GUIDE_STACK_2':
+      return PUERTO_RICO_GUIDE_STACK_2;
+    case 'PUERTO_RICO_GUIDE_STACK_3':
+      return PUERTO_RICO_GUIDE_STACK_3;
+    case 'PUERTO_RICO_GUIDE_STACK_4':
+      return PUERTO_RICO_GUIDE_STACK_4;
+    case 'PUERTO_RICO_GUIDE_STACK_5':
+      return PUERTO_RICO_GUIDE_STACK_5;
+    case 'PUERTO_RICO_GUIDE_STACK_6':
+      return PUERTO_RICO_GUIDE_STACK_6;
+    case 'PUERTO_RICO_GUIDE_STACK_7':
+      return PUERTO_RICO_GUIDE_STACK_7;
+    case 'PUERTO_RICO_GUIDE_STACK_8':
+      return PUERTO_RICO_GUIDE_STACK_8;
+    case 'PUERTO_RICO_GUIDE_SLIDER_1':
+      return PUERTO_RICO_GUIDE_SLIDER_1;
+    case 'PUERTO_RICO_GUIDE_SLIDER_2':
+      return PUERTO_RICO_GUIDE_SLIDER_2;
+    case 'PUERTO_RICO_GUIDE_SLIDER_3':
+      return PUERTO_RICO_GUIDE_SLIDER_3;
+    case 'PUERTO_RICO_GUIDE_SLIDER_4':
+      return PUERTO_RICO_GUIDE_SLIDER_4;
+    case 'PUERTO_RICO_GUIDE_SLIDER_5':
+      return PUERTO_RICO_GUIDE_SLIDER_5;
+    case 'PUERTO_RICO_GUIDE_SLIDER_6':
+      return PUERTO_RICO_GUIDE_SLIDER_6;
+    case 'PUERTO_RICO_GUIDE_SLIDER_7':
+      return PUERTO_RICO_GUIDE_SLIDER_7;
+
     case 'WE_PATH_TITLE':
       return WE_PATH_TITLE;
     case 'WE_PATH_STACK_1':
@@ -281,6 +417,173 @@ export const projectImageMapper = (imageName) => {
       return WE_PATH_STACK_4;
     case 'WE_PATH_STACK_5':
       return WE_PATH_STACK_5;
+    case 'WE_PATH_STACK_6':
+      return WE_PATH_STACK_6;
+
+    case 'AC_NAUTICA_TITLE':
+      return AC_NAUTICA_TITLE;
+    case 'AC_NAUTICA_STACK_1':
+      return AC_NAUTICA_STACK_1;
+    case 'AC_NAUTICA_STACK_2':
+      return AC_NAUTICA_STACK_2;
+    case 'AC_NAUTICA_STACK_3':
+      return AC_NAUTICA_STACK_3;
+    case 'AC_NAUTICA_STACK_4':
+      return AC_NAUTICA_STACK_4;
+    case 'AC_NAUTICA_STACK_5':
+      return AC_NAUTICA_STACK_5;
+    case 'AC_NAUTICA_SLIDER_1':
+      return AC_NAUTICA_SLIDER_1;
+    case 'AC_NAUTICA_SLIDER_2':
+      return AC_NAUTICA_SLIDER_2;
+    case 'AC_NAUTICA_SLIDER_3':
+      return AC_NAUTICA_SLIDER_3;
+    case 'AC_NAUTICA_SLIDER_4':
+      return AC_NAUTICA_SLIDER_4;
+    case 'AC_NAUTICA_SLIDER_5':
+      return AC_NAUTICA_SLIDER_5;
+    case 'AC_NAUTICA_SLIDER_6':
+      return AC_NAUTICA_SLIDER_6;
+
+    case 'ALIBALANCE_TITLE':
+      return ALIBALANCE_TITLE;
+    case 'ALIBALANCE_STACK_1':
+      return ALIBALANCE_STACK_1;
+    case 'ALIBALANCE_STACK_2':
+      return ALIBALANCE_STACK_2;
+    case 'ALIBALANCE_STACK_3':
+      return ALIBALANCE_STACK_3;
+    case 'ALIBALANCE_STACK_4':
+      return ALIBALANCE_STACK_4;
+    case 'ALIBALANCE_STACK_5':
+      return ALIBALANCE_STACK_5;
+
+    case 'GREEN_CLINIC_TITLE':
+      return GREEN_CLINIC_TITLE;
+    case 'GREEN_CLINIC_STACK_1':
+      return GREEN_CLINIC_STACK_1;
+    case 'GREEN_CLINIC_STACK_2':
+      return GREEN_CLINIC_STACK_2;
+    case 'GREEN_CLINIC_STACK_3':
+      return GREEN_CLINIC_STACK_3;
+    case 'GREEN_CLINIC_STACK_4':
+      return GREEN_CLINIC_STACK_4;
+    case 'GREEN_CLINIC_SLIDER_1':
+      return GREEN_CLINIC_SLIDER_1;
+    case 'GREEN_CLINIC_SLIDER_2':
+      return GREEN_CLINIC_SLIDER_2;
+    case 'GREEN_CLINIC_SLIDER_3':
+      return GREEN_CLINIC_SLIDER_3;
+    case 'GREEN_CLINIC_SLIDER_4':
+      return GREEN_CLINIC_SLIDER_4;
+    case 'GREEN_CLINIC_SLIDER_5':
+      return GREEN_CLINIC_SLIDER_5;
+    case 'GREEN_CLINIC_SLIDER_6':
+      return GREEN_CLINIC_SLIDER_6;
+    case 'GREEN_CLINIC_SLIDER_7':
+      return GREEN_CLINIC_SLIDER_7;
+    case 'GREEN_CLINIC_SLIDER_8':
+      return GREEN_CLINIC_SLIDER_8;
+    case 'GREEN_CLINIC_SLIDER_9':
+      return GREEN_CLINIC_SLIDER_9;
+
+    case 'ITBI_TITLE':
+      return ITBI_TITLE;
+    case 'ITBI_STACK_1':
+      return ITBI_STACK_1;
+    case 'ITBI_STACK_2':
+      return ITBI_STACK_2;
+    case 'ITBI_STACK_3':
+      return ITBI_STACK_3;
+    case 'ITBI_STACK_4':
+      return ITBI_STACK_4;
+    case 'ITBI_STACK_5':
+      return ITBI_STACK_5;
+    case 'ITBI_SLIDER_1':
+      return ITBI_SLIDER_1;
+    case 'ITBI_SLIDER_2':
+      return ITBI_SLIDER_2;
+    case 'ITBI_SLIDER_3':
+      return ITBI_SLIDER_3;
+    case 'ITBI_SLIDER_4':
+      return ITBI_SLIDER_4;
+    case 'ITBI_SLIDER_5':
+      return ITBI_SLIDER_5;
+
+    case 'SKILLSHAPE_TITLE':
+      return SKILLSHAPE_TITLE;
+    case 'SKILLSHAPE_STACK_1':
+      return SKILLSHAPE_STACK_1;
+    case 'SKILLSHAPE_STACK_2':
+      return SKILLSHAPE_STACK_2;
+    case 'SKILLSHAPE_STACK_3':
+      return SKILLSHAPE_STACK_3;
+    case 'SKILLSHAPE_STACK_4':
+      return SKILLSHAPE_STACK_4;
+    case 'SKILLSHAPE_STACK_5':
+      return SKILLSHAPE_STACK_5;
+    case 'SKILLSHAPE_STACK_6':
+      return SKILLSHAPE_STACK_6;
+    case 'SKILLSHAPE_STACK_7':
+      return SKILLSHAPE_STACK_7;
+    case 'SKILLSHAPE_SLIDER_1':
+      return SKILLSHAPE_SLIDER_1;
+    case 'SKILLSHAPE_SLIDER_2':
+      return SKILLSHAPE_SLIDER_2;
+    case 'SKILLSHAPE_SLIDER_3':
+      return SKILLSHAPE_SLIDER_3;
+    case 'SKILLSHAPE_SLIDER_4':
+      return SKILLSHAPE_SLIDER_4;
+    case 'SKILLSHAPE_SLIDER_5':
+      return SKILLSHAPE_SLIDER_5;
+
+    case 'STTM_DESKTOP_TITLE':
+      return STTM_DESKTOP_TITLE;
+    case 'STTM_DESKTOP_STACK_1':
+      return STTM_DESKTOP_STACK_1;
+    case 'STTM_DESKTOP_STACK_2':
+      return STTM_DESKTOP_STACK_2;
+    case 'STTM_DESKTOP_STACK_3':
+      return STTM_DESKTOP_STACK_3;
+    case 'STTM_DESKTOP_STACK_4':
+      return STTM_DESKTOP_STACK_4;
+    case 'STTM_DESKTOP_STACK_5':
+      return STTM_DESKTOP_STACK_5;
+
+    case 'DEMOBUTLER_TITLE':
+      return DEMOBUTLER_TITLE;
+    case 'DEMOBUTLER_STACK_1':
+      return DEMOBUTLER_STACK_1;
+    case 'DEMOBUTLER_STACK_2':
+      return DEMOBUTLER_STACK_2;
+    case 'DEMOBUTLER_STACK_3':
+      return DEMOBUTLER_STACK_3;
+    case 'DEMOBUTLER_STACK_4':
+      return DEMOBUTLER_STACK_4;
+    case 'DEMOBUTLER_STACK_5':
+      return DEMOBUTLER_STACK_5;
+    case 'DEMOBUTLER_STACK_6':
+      return DEMOBUTLER_STACK_6;
+    case 'DEMOBUTLER_STACK_7':
+      return DEMOBUTLER_STACK_7;
+    case 'DEMOBUTLER_STACK_8':
+      return DEMOBUTLER_STACK_8;
+    case 'DEMOBUTLER_STACK_9':
+      return DEMOBUTLER_STACK_9;
+    case 'DEMOBUTLER_STACK_10':
+      return DEMOBUTLER_STACK_10;
+    case 'DEMOBUTLER_STACK_11':
+      return DEMOBUTLER_STACK_11;
+    case 'DEMOBUTLER_STACK_12':
+      return DEMOBUTLER_STACK_12;
+    case 'DEMOBUTLER_STACK_13':
+      return DEMOBUTLER_STACK_13;
+    case 'DEMOBUTLER_STACK_14':
+      return DEMOBUTLER_STACK_14;
+    case 'DEMOBUTLER_STACK_15':
+      return DEMOBUTLER_STACK_15;
+    case 'DEMOBUTLER_STACK_16':
+      return DEMOBUTLER_STACK_16;
     default:
       return FORBY_STACK_4;
   }
