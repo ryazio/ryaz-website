9d960b6d421c737fba6597c5488708b6967c6192
Sanchit987 <kherasanchit1@gmail.com>
2022-05-12 13:59:34 +0530

    Footer finalized

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,20 +10,20 @@ export const Footer = () => (
       <section>
         <div className="footer-logo-holder">
           <RyazLogo className="footer-logo" />
-          <p className="footer-logo-name">RYAZ.IO</p>
+          {/* <p className="footer-logo-name">RYAZ.IO</p> */}
         </div>
-        <p>#250, Ward no. 2, Near water tank Samrala 141114 IN</p>
+        <p className="footer-text-address">#250, Ward no. 2, Near water tank Samrala 141114 IN</p>
         <p>+91 6280256990</p>
       </section>
       <section>
-        <p><Link to="/">Home</Link></p>
-        <p><Link to="/about">About Us</Link></p>
-        <p><Link to="/work">Our Work</Link></p>
-        <p><Link to="/contact">Contact</Link></p>
+        <p className="footer-text"><Link to="/">Home</Link></p>
+        <p className="footer-text"><Link to="/about">About Us</Link></p>
+        <p className="footer-text"><Link to="/work">Our Work</Link></p>
+        <p className="footer-text"><Link to="/contact">Contact</Link></p>
       </section>
       <section>
-        <p><Link to="/contact">Hire Us</Link></p>
-        <p><Link to="/contact">Join Us</Link></p>
+        <p className="footer-text"><Link to="/contact">Hire Us</Link></p>
+        <p className="footer-text"><Link to="/contact">Join Us</Link></p>
         <SocialLinks className="footer-links-desktop" />
       </section>
       <SocialLinks className="footer-links-mobile" />
