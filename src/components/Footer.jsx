2cc0f21ea31f69152114a4185efeeb1734361b63
Khushi Aggarwal <khushiaggarwal1302@gmail.com>
2025-03-11 17:12:11 +0530

    fix: minor issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,19 +12,34 @@ export const Footer = () => (
           <RyazLogo className="footer-logo" />
           {/* <p className="footer-logo-name">RYAZ.IO</p> */}
         </div>
-        <p className="footer-text-address">H. No. 188/2, Near Raksha Madam, Water Tank Road, Samrala, Ludhiana, Punjab, 141114 IN</p>
+        <p className="footer-text-address">
+          H. No. 188/2, Near Raksha Madam, Water Tank Road, Samrala, Ludhiana,
+          Punjab, 141114 IN
+        </p>
         <p>+91 6280256990</p>
         <p>inder@ryaz.io</p>
       </section>
       <section>
-        <p className="footer-text"><a href="/">Home</a></p>
-        <p className="footer-text"><a href="/#about">About Us</a></p>
-        <p className="footer-text"><a href="/#work">Our Work</a></p>
-        <p className="footer-text"><a href="/#contact">Contact</a></p>
+        <p className="footer-text">
+          <a href="/">Home</a>
+        </p>
+        <p className="footer-text">
+          <a href="/about">About Us</a>
+        </p>
+        <p className="footer-text">
+          <a href="/work">Our Work</a>
+        </p>
+        <p className="footer-text">
+          <a href="/contact">Contact</a>
+        </p>
       </section>
       <section>
-        <p className="footer-text"><a href="/#contact">Hire Us</a></p>
-        <p className="footer-text"><a href="/#contact">Join Us</a></p>
+        <p className="footer-text">
+          <a href="/contact">Hire Us</a>
+        </p>
+        <p className="footer-text">
+          <a href="/contact">Join Us</a>
+        </p>
         <SocialLinks className="footer-links-desktop" />
       </section>
       <SocialLinks className="footer-links-mobile" />
